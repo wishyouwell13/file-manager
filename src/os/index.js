7c0eb229b775d119ffa6881adcb114f518ab3bf5
Alexey <alexey@MacBook-Pro-Alexey.local>
2023-06-22 22:21:51 +0300

    refactor: rewrite os module

--- src/os/index.js
+++ src/os/index.js
@@ -1,17 +1,44 @@
 import os from 'os';
 
-export const getOsInfo = (param) => {
-  param = normalizeParam(param);
-  if (param in os) {
-    const res = typeof os[param] === 'function' ? os[param]() : os[param];
-  } else {
-    throw new Error();
-  }
-};
-function normalizeParam(str) {
-  const param = str.replace(/--(\w+)/, (_, command) =>
-    command === 'architecture' ? 'arch' : command,
-  );
+export const getOsInfo = (arg) => {
+  const commandsObj = {
+    EOL: () => os.EOL,
+    homedir: () => os.homedir(),
+    architecture: () => os.arch(),
+    username: () => {
+      const { username } = os.userInfo();
+      return username;
+    },
+    homedir: () => {
+      const { homedir } = os.userInfo();
+      return homedir;
+    },
+    cpus: () => {
+      const cores = os.cpus();
+      const result = {
+        'amount of CPUS': cores.length,
+      };
+      // amount of CPUS
+      // model and clock rate (in GHz) for each of them
+      cores.forEach((core, idx) => {
+        result[`core ${idx}`] = {
+          model: core.model,
+          speed: core.speed + ' GHz',
+        };
+      });
 
-  return param;
-}
+      return result;
+    },
+  };
+  const osInfo = commandsObj[arg];
+  return osInfo();
+};
+// function normalizeParam(str) {
+//   const param = str.replace(/--(\w+)/, (_, command) =>
+//     command === 'architecture' ? 'arch' : command,
+//   );
+
+//   return param;
+// }
+
+// console.log(getOsInfo('homedir'));
